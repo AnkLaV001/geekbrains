fdd5836928119de2057a632634ccd656f4f145b8
AndrE <aanklav@gmail.com>
2021-02-08 23:40:30 +0300

    Начал ДЗ3

---
+++ Java/Lesson1/src/Lesson3.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+
+    }
+    public static char[][] map;
+    public static int sizeMapX;
+    public static int sizeMapY;
+
+    public static char player1 = 'X';
+    public static char player2 = 'O';
+    public static char emptyPlace = '_';
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+}
