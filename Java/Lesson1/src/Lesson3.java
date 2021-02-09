93e98420d584ec9bc357648d3462dab92742ea86
AndrE <aanklav@gmail.com>
2021-02-09 08:55:51 +0300

    делаю3ДЗ

--- Java/Lesson1/src/Lesson3.java
+++ Java/Lesson1/src/Lesson3.java
@@ -2,9 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson3 {
-    public static void main(String[] args) {
 
-    }
     public static char[][] map;
     public static int sizeMapX;
     public static int sizeMapY;
@@ -15,4 +13,38 @@ public class Lesson3 {
 
     public static Scanner scanner = new Scanner(System.in);
     public static Random random = new Random();
+
+    public static void createMap(){
+        sizeMapY = 3;
+        sizeMapX = 3;
+        map = new char[sizeMapY][sizeMapX];
+        for (int y = 0; y < sizeMapY; y++){
+            for (int x = 0; x < sizeMapX; x++) {
+                map[y][x] = emptyPlace;
+            }
+        }
+    }
+
+    public static void printMap(){
+        for (int y = 0; y < sizeMapY; y++) {
+            for (int x = 0; x < sizeMapX; x++) {
+                System.out.print(map[y][x] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("_________________");
+    }
+
+    public static boolean checkMapSize(int x, int y){
+        return x >= 0 && x < sizeMapX && y >= 0 && y < sizeMapY;
+    }
+    public static boolean checkEmptyPlace(int x, int y){
+        return map[y][x] == emptyPlace;
+    }
+
+    public static void main(String[] args) {
+        createMap();
+        printMap();
+    }
+
 }
