eed0498c0d38d1afc31b893b146c136e20e983f3
AndrE <aanklav@gmail.com>
2021-02-10 08:39:23 +0300

    доработка просчёта победы для ИИ

--- Java/Lesson1/src/Lesson3.java
+++ Java/Lesson1/src/Lesson3.java
@@ -35,16 +35,99 @@ public class Lesson3 {
         System.out.println("_________________");
     }
 
+    public static void player1Turn(){
+        int x;
+        int y;
+        do{
+            System.out.println("Введите позицию куда поставить фишку:");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        }   while (!checkMapSize(y, x) || !checkEmptyPlace(y,x));
+        map[y][x] = player1;
+    }
+
+    public static void player2Turn(){
+        int x;
+        int y;
+        do{
+//            System.out.println("Введите позицию куда поставить фишку:");
+//            x = scanner.nextInt() - 1;
+//            y = scanner.nextInt() - 1;
+            if(){
+
+            }
+            x = random.nextInt(sizeMapX);
+            y = random.nextInt(sizeMapY);
+        }   while (!checkEmptyPlace(y,x));
+        map[y][x] = player2;
+    }
+
     public static boolean checkMapSize(int x, int y){
         return x >= 0 && x < sizeMapX && y >= 0 && y < sizeMapY;
     }
     public static boolean checkEmptyPlace(int x, int y){
         return map[y][x] == emptyPlace;
     }
+    public static boolean checkOpponentWin(char anotherPlayer){
+        for (int y = 0; y < sizeMapY; y++) {
+            for (int x = 0; x < sizeMapX; x++) {
+                if(map[y][x] = anotherPlayer && checkWin(anotherPlayer))
+            }
+
+        }
+    }
+    public static boolean checkWin(char currentPlayer){
+        if (map[0][0] == currentPlayer && map[0][1] == currentPlayer && map[0][2] == currentPlayer) return true;
+        if (map[1][0] == currentPlayer && map[1][1] == currentPlayer && map[1][2] == currentPlayer) return true;
+        if (map[2][0] == currentPlayer && map[2][1] == currentPlayer && map[2][2] == currentPlayer) return true;
+
+        if (map[0][0] == currentPlayer && map[1][0] == currentPlayer && map[2][0] == currentPlayer) return true;
+        if (map[0][1] == currentPlayer && map[1][1] == currentPlayer && map[2][1] == currentPlayer) return true;
+        if (map[0][2] == currentPlayer && map[1][2] == currentPlayer && map[2][2] == currentPlayer) return true;
+
+        if (map[0][0] == currentPlayer && map[1][1] == currentPlayer && map[2][2] == currentPlayer) return true;
+        if (map[0][2] == currentPlayer && map[1][1] == currentPlayer && map[2][0] == currentPlayer) return true;
+
+        return false;
+    }
+    public static boolean checkFullMap(){
+        for (int y = 0; y < sizeMapY; y++) {
+            for (int x = 0; x < sizeMapX; x++) {
+                if (map[y][x] == emptyPlace){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         createMap();
         printMap();
+
+        while (true) {
+            player1Turn();
+            printMap();
+            if (checkWin(player1)) {
+                System.out.println("Human WIN!");
+                break;
+            }
+            if (checkFullMap()) {
+                System.out.println("DRAW!");
+                break;
+            }
+
+            player2Turn();
+            printMap();
+            if (checkWin(player2)) {
+                System.out.println("AI WIN!");
+                break;
+            }
+            if (checkFullMap()) {
+                System.out.println("DRAW!");
+                break;
+            }
+        }
     }
 
 }
