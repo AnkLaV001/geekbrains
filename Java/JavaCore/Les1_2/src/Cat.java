3c50147370ccfbe4d4f9c9196f695982752d91e0
AnkLaV001 <aanklav@gmail.com>
2021-03-19 19:19:56 +0300

    HW1

---
+++ Java/JavaCore/Les1_2/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+    private int lengthOfRun;
+    private int heightOfJump;
+
+    public Cat(String name, int lengthOfRun, int heightOfJump){
+        this.name = name;
+        this.lengthOfRun = lengthOfRun;
+        this.heightOfJump = heightOfJump;
+    }
+}
