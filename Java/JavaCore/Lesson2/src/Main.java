1d6da6fb3c12ca9f1ef842f30c8d839b8976b2c3
AnkLaV001 <aanklav@gmail.com>
2021-03-23 18:34:05 +0300

    Core HW3

--- Java/JavaCore/Lesson2/src/Main.java
+++ Java/JavaCore/Lesson2/src/Main.java
@@ -1,21 +1,21 @@
 import java.lang.reflect.Array;
 
 public class Main {
-    public static void main(String[] args) throws MyArrayDataException, MyArraySizeException{
+    public static void main(String[] args) throws MyArrayDataException {
 
 
         try {
             System.out.println(createArray(new String[][]{{"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "5", "3", "4"}, {"1", "2", "3", "4"}}));
         } catch (MyArraySizeException e) {
             e.printStackTrace();
-        } catch (MyArrayDataException e){
+        } catch (MyArrayDataException e) {
             System.out.println("Ошибка в строке: " + e.getI() + ", столбце: " + e.getJ());
         }
 
 
     }
 
-    public static int createArray(String[][] array) throws MyArraySizeException, MyArrayDataException{
+    public static int createArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
         int a = array.length;
         int b = array[0].length;
         int summ = 0;
