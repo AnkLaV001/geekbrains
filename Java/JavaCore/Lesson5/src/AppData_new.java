a01bde358a3e86cb2353efe6cdd91c9760376de9
AndrE <aanklav@gmail.com>
2021-04-06 08:55:43 +0300

    JavaCore 5HW

--- Java/JavaCore/Lesson5/src/AppData_new.java
+++ Java/JavaCore/Lesson5/src/AppData_new.java
@@ -1,12 +1,12 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class AppData_new {
-    private String[] header = {"Value 1;", "Value 2;", "Value 3"};
+    private String[] header;
     private int[][] data;
 
-    public AppData_new(int[][] data) {
+    public AppData_new(String[] header, int[][] data) {
         this.data = data;
     }
 
@@ -15,7 +15,7 @@ public class AppData_new {
         return Arrays.toString(header) + Arrays.deepToString(data);
     }
 
-    public void saveCSV(){
+    public void saveCSV(String[] header){
         try(PrintWriter out = new PrintWriter("lesson5.scv")){
             for(String headerOut : header){
                 out.print(headerOut);
@@ -23,10 +23,35 @@ public class AppData_new {
             }
             out.println("");
             for (int[] dataOut : data){
-                out.print(dataOut);
+                out.print(Arrays.toString(dataOut));
                 out.println("");
             }
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readCSV (int[][] dataArray){
+        ArrayList<AppData_new> appData_new = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("lesson5.scv"))) {
+            String tempString;
+            while ((tempString = bufferedReader.readLine()) != null){
+                String[] headers = tempString.split(";");
+            }
+            String tempIntString;
+            int[][] intArray = new int[2][3];
+            int i=0;
+            while ((tempIntString = bufferedReader.readLine()) !=null){
+                String[] valuesArray = tempIntString.split(";");
+                i++;
+                intArray[i][0] = Integer.parseInt(valuesArray[0]);
+                intArray[i][1] = Integer.parseInt(valuesArray[1]);
+                intArray[i][2] = Integer.parseInt(valuesArray[2]);
+            }
+            System.out.println(tempString + tempIntString);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- Java/JavaCore/Lesson5/src/Main.java
+++ Java/JavaCore/Lesson5/src/Main.java
@@ -1,11 +1,17 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        String[] headerArray = {"Value 1;", "Value 2;", "Value 3"};
        int[][] dataArray = {{423, 432, 435}, {423, 4324, 5435}};
+       int[][] dataArray2 = {{4754, 77, 111}, {22, 543, 2234}};
+
 //        AppData appData = new AppData(dataArray);
 //
 //        appData.saveCSV("Value");
 
-        AppData_new appData_new = new AppData_new(dataArray);
-        appData_new.saveCSV();
+        AppData_new appData_new = new AppData_new(headerArray, dataArray);
+        appData_new.saveCSV(headerArray);
+        appData_new.readCSV(dataArray2);
     }
 }
